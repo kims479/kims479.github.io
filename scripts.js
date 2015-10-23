f0da1339b7561682218bde5090897be45278ed81
kims479 <kims479@newschool.edu>
2015-10-23 15:42:24 +0000

    trees

--- scripts.js
+++ scripts.js
@@ -1,19 +1,30 @@
-var symbols1 = ["~","~","~","~","~","~","~","~","~","~","~","~","~","~","~","~","~","~","~","~","~","~","~","~","~"];
-var symbols2 = ["!","!","!","!","!","!","!","!","!","!","!","!","!","!","!","!","!","!","!","!","!","!","!","!","!"];
+var symbols1 = [];
+var symbols2 = [];
+var symbols3 = [];
 
-var pattern = "~";
-var star ="*";
+var star1 = "~";
+var star2 = "*";
+var star3 = "!";
 
-  for (var increment = 0; increment < symbols1.length; increment++) {
-    pattern = pattern + symbols1[increment];
+  for (var increment = 0; increment < 45; increment++) {
+    symbols1.push(star1);
+    star1 = star1 + "~";
+    symbols1.reverse();
   }
 
-  for (var increment = 0; increment < symbols2.length; increment++) {
-    star = star + symbols2[increment];
+  for (var increment = 0; increment < 45; increment++) {
+    symbols2.push(star2);
+    star2 = star2 + "*" + "*";
   }
-
-    symbols2.reverse();
+  
+  for (var increment = 0; increment < 50; increment++) {
+    symbols3.push(star3);
+    star3 = star3 + "!";
+  }
+  
+        
+        symbols3.reverse();
     
-  for (var increment = 0; increment < symbols1.length; increment++) {
-    console.log(symbols1[increment],symbols2[increment],symbols1[increment],symbols2[increment],symbols1[increment],symbols2[increment],symbols1[increment],symbols2[increment]);
+  for (var increment = 0; increment < 20; increment++) {
+    console.log(symbols3[increment],symbols2[increment],symbols1[increment],symbols2[increment],symbols3[increment]);
   }
