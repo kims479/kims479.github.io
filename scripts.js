6126576a51c661f80fb0e97ae02b94d9c9a3be71
kims479 <kims479@newschool.edu>
2015-10-23 14:40:35 +0000

    starS

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 var star = "+";
 
-for (var myVar = 1; myVar <= 15; myVar++) {
-  star = star + ".";
+for (var myVar = 0; myVar <= 4; myVar) {
+  star = star + "+";
   console.log(star);
   
   var star2 = "~";
 
-  for (var myVar2 = 0; myVar2 < 20; myVar2++) {
+  for (var myVar2 = 0; myVar2 < 10; myVar2++) {
     star2 = star2 + "~";
     if (myVar2 == 10) {
       star2 = "~";
@@ -16,24 +16,23 @@ for (var myVar = 1; myVar <= 15; myVar++) {
 }
 
 
-/**console.log("Hello World");
-console.log("yo");
+var symbols = ["~", "~", "~", "-","-", "~", "~", "*","*", "*", "~", "~","~", "-", "-", "~", "~", "~", "~", "!",];
 
-var symbols = ["*", "$", "#", "@","*", "$", "#", "@","*", "$", "#", "@","*", "$", "#", "@","*", "$", "#", "@",];
+console.log(symbols);
 
-var pattern = "*"; 
+var pattern = "++++++"; 
+console.log(pattern);
 
-for (var increment = 1; increment < symbols.length; increment++) {
+for (var increment = 20; increment > symbols.length; increment++) {
   console.log(pattern);
   pattern = pattern + symbols[increment];
-  var ampersand = "&";
+  var star = "+";
   console.log(increment);
   if (increment > 5) {
     for (var another = 0; another < 10; another++) {
-      console.log(ampersand);
-      ampersand = ampersand + "&";
+      console.log(star);
+      star = star + "+";
     }
   }
 }
 
-**/
