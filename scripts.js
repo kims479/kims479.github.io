0d23881b60e8a5f5ba191b8d49f0698f6eacf361
kims479 <kims479@newschool.edu>
2015-10-23 12:55:43 +0000

    new

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
 /**example**/
-var star = "*";
+var star = ".";
 
-for (var myVar = 1; myVar <= 10; myVar++) {
-  star = star + "*";
+for (var myVar = 8; myVar <= 10; myVar++) {
+  star = star + ".";
   console.log(star);
-  var star2 = "R";
+  
+  var star2 = "";
 
   for (var myVar2 = 0; myVar2 < 20; myVar2++) {
     star2 = star2 + "R";
@@ -15,6 +16,23 @@ for (var myVar = 1; myVar <= 10; myVar++) {
   }
 }
 
+........................,,-~*~,,
+......................./:.:.:.:.:.|
+......................|;.;.;.;.;./
+......................|.;.;.;.;.|
+............._,,,,,_.).;.;.;.;.|
+.........,,-":.:.:.:."~-,;.;.;.|
+........(_,,,,---,,_:.:.);.;.;..",,
+......,-":.:.:.:.:.""-,,/;.;.;.;.;.",
+.....(:.__,,,,,,,,,___);.;.;.;.;.;|
+...../"":.:.:.:.:.:.:¯""\;.;.;.;.;.,"
+....\",__,,,,,,,,,,,__/;;;;;;;;;/\
+.....\.::.:.:.:.:.:.:.;.);;;;;;;;;/:\
+.......\,,,,,---~~~~;;;;;;;;,"::::\
+.........."""~~--,,,,,,,,,,-"::::::::::\
+...................\::::::::::::::::::::::\
+
+
 console.log("Hello World");
 console.log("yo");
 
@@ -22,8 +40,16 @@ var symbols = ["*", "$", "#", "@","*", "$", "#", "@","*", "$", "#", "@","*", "$"
 
 var pattern = "*"; 
 
-for (var increment = 1; increment < 20; increment++) {
+for (var increment = 1; increment < symbols.length; increment++) {
   console.log(pattern);
   pattern = pattern + symbols[increment];
+  var ampersand = "&";
+  console.log(increment);
+  if (increment > 5) {
+    for (var another = 0; another < 10; another++) {
+      console.log(ampersand);
+      ampersand = ampersand + "&";
+    }
+  }
 }
 
