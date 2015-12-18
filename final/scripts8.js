44b0adf7318b442f026d74cccf73c0c60ab12eb0
kims479 <kims479@newschool.edu>
2015-12-18 10:37:41 +0000

    finallydone

--- final/scripts8.js
+++ final/scripts8.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
     
     $('#change').click(function(){
+        
         var index = Math.floor((Math.random() * 4) + 1);
         
         if(index==1) {
+          $("#change").html("nah..how about something else?");
+          $("#person").hide();
           $('#one').show();
           $('#two').hide();
           $('#three').hide();
@@ -11,6 +14,8 @@ $(document).ready(function(){
         }
         
         if(index==2) {
+           $("#change").html("hmm..let's do something else");
+           $("#person").hide();
            $('#two').show();
            $('#one').hide();
            $('#three').hide();
@@ -18,6 +23,8 @@ $(document).ready(function(){
         }
         
         if(index==3) {
+            $("#change").html("i think i rather not do that");
+            $("#person").hide();
             $('#three').show();
             $('#one').hide();
             $('#two').hide();
@@ -25,14 +32,15 @@ $(document).ready(function(){
         }
         
         if(index==4) {
+            $("#change").html("maybe this..or not");
+            $("#person").hide();
             $('#four').show();
             $('#one').hide();
             $('#two').hide();
             $('#three').hide();
         }
     });
+    
        
 });
-
-
 
