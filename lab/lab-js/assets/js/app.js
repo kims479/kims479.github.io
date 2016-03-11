73ec21401a13cadb2666b9257fa053533a419c31
Alexa <kims479@newschool.edu>
2016-03-10 21:00:56 -0500

    dag

--- lab/lab-js/assets/js/app.js
+++ lab/lab-js/assets/js/app.js
@@ -1,60 +1,49 @@
-//function
-//alert('hello, js');
+// function
+// alert('hello js');
 
 //variables
 var animal = "cat";
-var age = 13;
+var age = 12.5;
 var clothing = ["shirts", "socks", "pants"];
 var winner = false;
 var checking_account = 1000;
-var marni_shirt = 1000;
-var marni_earrings = 450;
-var stella_dress = 3000;
 
 console.log(winner);
 
-//fucntions
+// functions
 function print_text(text) {
-	$('.value').html(text);
+  $('.value').html(text);
 }
+
 
 function charge_cc(item_value, brand_name) {
-	alert('You will be charged ' + item_value + ' for this' + item);
-	alert('Your checking account balance is ' + checking_account);
+  alert('You will be charged ' + item_value + ' for this ' + brand_name + ' item');
+  alert('Your checking account balance is ' + checking_account);
 
-	if (checking_account < item_value) {
-		if (checking_account < 1) {
-			alert("You don't have any money");
-		} else {
-			alert("you don't have enough money to purchase this item");
-	}	
-
-	} else {
-		checking_account = checking_account - item_value;
-		alert('Charge successful');
-		alert('Checking balance is ' + checking_account);
-	}	
-
-	// the value of the thing we're buying
-	// checking account balance
+  if (checking_account < item_value) {
+    if (checking_account < 1) {
+      alert('You dont have any money');
+    } else {
+      alert('You dont have enough money to purchase this item');
+    }
+  } else {
+    checking_account = checking_account - item_value;
+    alert('Charge successful');
+    display_checking_balance();
+    alert('Checking balance is ' + checking_account);
+  }
 }
 
-if (winner == true) {
-	alert('you are awesome!');
-} else {
-	alert('you suck');
-}
-
-if (age > 7) {
-	alert('enjoy mighty ducks');
-} else if (age < 14) {
-	alert('spotlight');
-} else {
-	alert('you are cute');
+function display_checking_balance() {
+  $('.value').html(checking_account);
 }
 
 $('.item').click(function() {
-	var item_value = $(this).data('value');
-	var brand_name = $(this).data('brand');
-	charge_cc(item_value, brand_name);
+  var item_value = $(this).data('value');
+  var brand_name = $(this).data('brand');
+  charge_cc(item_value, brand_name);
 });
+
+display_checking_balance();
+
+
