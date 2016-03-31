f720503d5c53fc678ca8d16dad3b9d9b2e4655d3
Alexa <kims479@newschool.edu>
2016-03-31 10:55:54 -0400

    a

---
+++ lab/lab-js2/assets/js/app.js
@@ -0,0 +1,21 @@
+$('.nav-item').click(function() {
+	$('.nav-item').removeClass('active');
+	$(this).addClass('active');
+	
+	var button_name = $(this).data('button');
+	
+	$('.content').attr('class', 'column content');
+	$('.content').addClass(button_name);
+
+	if (button_name == 'beyonce') {
+		$('.slay').removeClass('hide');
+		$('audio')[0].play();
+	}
+	
+});
+
+$('.close').click(function() {
+	debugger;
+	$('.slay').addClass('hide');
+	$('.nav-item').removeClass('active')
+});
