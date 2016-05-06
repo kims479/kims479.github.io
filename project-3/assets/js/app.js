47532588162983cc2fe7cd98821756ca5a610068
Alexa <kims479@newschool.edu>
2016-05-06 13:46:40 -0400

    dhas

--- project-3/assets/js/app.js
+++ project-3/assets/js/app.js
@@ -2,6 +2,8 @@ for (i=0; i<6000; i++) {
   $("body").append("<div class='box'></div>");
 }
 
+var isActive = false;
+
 var currentColor = "white";
 
 $(".pink").click(function() {
@@ -45,7 +47,9 @@ $(".black").click(function() {
 });
 
 $(".box").click(function() {
+  if (isActive) {
   $(this).css("background-color",currentColor);
+}
 });
 
 if (window.location.hash == '#colors') {
@@ -59,17 +63,19 @@ function clear_board() {
   $("body").css("background-color","black");
   $(".box").css("background-color","transparent"); 
   $(".done").css("visibility","hidden");
-  $(".refresh").css("visibility","hidden");
+  $(".next").css("visibility","hidden");
 }
 
 $(".done").click(function() {
   $(this).css("visibility","hidden");
-  $(".refresh").css("visibility","visible");
+  $(".next").css("visibility","visible");
   $(".box").css("border","1px solid transparent");
   $("body").css("background-color","white");
   $(".color-picker").css('background-color', 'transparent');
   $(".color-picker").css('border', '1px solid transparent');
   $(".clear").css("visibility","hidden");
+  isActive = false;
+  //$(".box").off("click","");
 });
 
 //begin
@@ -79,10 +85,19 @@ $(".button2").click(function()  {
   $(".color-picker").attr('style', '');
   // $(".colors").css
   $(".done").css("visibility","visible");
-  $(".refresh").css("visibility","hidden");
+  $(".next").css("visibility","hidden");
+  $(".box").on("click","");
+  isActive = true;
 });
 
 //home
 $(".button3").click(function() {
   clear_board();
+});
+
+//cleear button
+$(".clear").click(function() {
+  $(".box").css("border","1px solid white");
+  $("body").css("background-color","black");
+  $(".box").css("background-color","transparent"); 
 });
